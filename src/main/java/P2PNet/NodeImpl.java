7db5062a631cc1df5a92daf1d2debf0086ac7cbf
Simone Richini <simone.richini@gmail.com>
2020-07-13 18:02:57 +0200

    Non so più nulla

--- src/main/java/P2PNet/NodeImpl.java
+++ src/main/java/P2PNet/NodeImpl.java
@@ -10,7 +10,6 @@ import org.glassfish.jersey.client.ClientConfig;
 import p2p.nodes.Node;
 import p2p.nodes.NodeServiceGrpc;
 
-import javax.ws.rs.ProcessingException;
 import javax.ws.rs.client.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -19,7 +18,10 @@ import java.util.List;
 
 public class NodeImpl extends NodeServiceGrpc.NodeServiceImplBase {
 
-    private static Integer sync;
+    private volatile boolean canReceiveToken;
+    private volatile boolean mustSendToken;
+    private static Integer syncToken;
+    private static Integer syncNode;
 
     private Integer id;
     private String ip;
@@ -34,20 +36,21 @@ public class NodeImpl extends NodeServiceGrpc.NodeServiceImplBase {
     //region Metodi grpc
     @Override
     public void changeNext(Node.NodeMessage request, StreamObserver<Node.OkMessage> responseObserver) {
-        synchronized (sync) {
+        synchronized (syncNode) {
             this.nextNodeId = request.getId();
             this.nextNodeIp = request.getIp();
             this.nextNodePort = request.getPort();
             Node.OkMessage response = Node.OkMessage.newBuilder().setVal("Next Node Changed for Node " + this.id.toString()).build();
             responseObserver.onNext(response);
             responseObserver.onCompleted();
-            sync.notify();
+            syncNode.notify();
         }
     }
 
     @Override
     public void recvToken (Node.Token request, StreamObserver<Node.OkMessage> responseObserver) {
-        try {
+        if (this.canReceiveToken) {
+            this.mustSendToken = true;
             List done = new ArrayList(request.getIdsList());
             List values = new ArrayList(request.getValuesList());
             int nodeConsidered = request.getNodesConsidered();
@@ -64,19 +67,26 @@ public class NodeImpl extends NodeServiceGrpc.NodeServiceImplBase {
                 values.clear();
                 done.clear();
             }
-            synchronized (sync) {
-                sendToken(nextNodeIp, nextNodePort, p2p.nodes.Node.Token.newBuilder().addAllIds(done).addAllValues(values).setNodesConsidered(nodeConsidered).build());
+            synchronized (syncToken) {
+                sendToken(this.nextNodeIp, this.nextNodePort, Node.Token.newBuilder().addAllIds(done).addAllValues(values).setNodesConsidered(nodeConsidered).build());
+                this.mustSendToken = false;
+                syncToken.notify();
             }
         }
-        catch (ProcessingException | InterruptedException e) { }
+        else {
+            Node.OkMessage response = Node.OkMessage.newBuilder().setVal("Error").build();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        }
     }
     //endregion
 
     //region Metodi
     public void exitFromNetwork () {
-        synchronized (sync) {
-            //if (this.mustSendToken)
-            //    try { sync.wait(); } catch (InterruptedException e) { }
+        synchronized (syncToken) {
+            while (this.mustSendToken)
+                try { syncToken.wait(); } catch (InterruptedException e) { }
+            this.canReceiveToken = false;
             Client client = ClientBuilder.newClient(new ClientConfig().register(LoggingFilter.class));
             WebTarget webTarget = client.target("http://localhost:8080/simple_service_webapp_war/webapi/nodes/remove_node");
             Beans.Node nodeBean = new Beans.Node(this.id,this.ip,this.port);
@@ -85,7 +95,7 @@ public class NodeImpl extends NodeServiceGrpc.NodeServiceImplBase {
             List<Beans.Node> nodes = response.readEntity(NodesList.class).getNodes();
             for (int i = 0; i < nodes.size(); i++)
                 if (nodes.get(i).getId().equals(this.nextNodeId))
-                    changeNext(nodes.get(mod((i-1),nodes.size())).getIp(),nodes.get(mod((i-1),nodes.size())).getPort(),nodes.get(i).getId(),nodes.get(i).getIp(),nodes.get(i).getPort());
+                    this.changeNext(nodes.get(mod((i-1),nodes.size())).getIp(),nodes.get(mod((i-1),nodes.size())).getPort(),nodes.get(i).getId(),nodes.get(i).getIp(),nodes.get(i).getPort());
         }
     }
 
@@ -99,8 +109,8 @@ public class NodeImpl extends NodeServiceGrpc.NodeServiceImplBase {
             List<Beans.Node> nodes = response.readEntity(NodesList.class).getNodes();
             for (int i = 0; i < nodes.size(); i++) {
                 if (nodes.get(i).getId().equals(this.id)){
-                    changeNext(nodes.get(mod((i-1),nodes.size())).getIp(),nodes.get(mod((i-1),nodes.size())).getPort(),this.id,this.ip,this.port);
-                    changeNext(this.ip,this.port,nodes.get(mod((i+1),nodes.size())).getId(),nodes.get(mod((i+1),nodes.size())).getIp(),nodes.get(mod((i+1),nodes.size())).getPort());
+                    this.changeNext(nodes.get(mod((i-1),nodes.size())).getIp(),nodes.get(mod((i-1),nodes.size())).getPort(),this.id,this.ip,this.port);
+                    this.changeNext(this.ip,this.port,nodes.get(mod((i+1),nodes.size())).getId(),nodes.get(mod((i+1),nodes.size())).getIp(),nodes.get(mod((i+1),nodes.size())).getPort());
                 }
             }
             if (nodes.size() == 1)
@@ -135,21 +145,27 @@ public class NodeImpl extends NodeServiceGrpc.NodeServiceImplBase {
     //endregion
 
     //region Client
-    public static void sendToken (String ip, Integer port, p2p.nodes.Node.Token token) throws InterruptedException {
-        try {
-            final ManagedChannel channel = ManagedChannelBuilder.forTarget(ip + ":" + port.toString()).usePlaintext(true).build();
-            NodeServiceGrpc.NodeServiceBlockingStub stub = NodeServiceGrpc.newBlockingStub(channel);
-            stub.recvToken(token);
-            channel.shutdown();
+    public void sendToken (String ip, Integer port, p2p.nodes.Node.Token token) {
+        synchronized (syncNode) {
+            try {
+                final ManagedChannel channel = ManagedChannelBuilder.forTarget(ip + ":" + port.toString()).usePlaintext(true).build();
+                NodeServiceGrpc.NodeServiceBlockingStub stub = NodeServiceGrpc.newBlockingStub(channel);
+                Node.OkMessage res = stub.recvToken(token);
+                channel.shutdown();
+                if (!res.getVal().equals("Ok"))
+                    throw new NullPointerException();
+            }
+            catch (NullPointerException e) {
+                try {
+                    syncNode.wait();
+                    this.sendToken(this.nextNodeIp,this.nextNodePort,token);
+                } catch (InterruptedException ex) { }
+            }
+            catch (StatusRuntimeException e) { }
         }
-        catch (NullPointerException e) {
-            sync.wait();
-            sendToken(ip,port,token);
-        }
-        catch (StatusRuntimeException e) { }
     }
 
-    public static void changeNext (String ip, Integer port, Integer newId, String newIp, Integer newPort){
+    public void changeNext (String ip, Integer port, Integer newId, String newIp, Integer newPort){
         final ManagedChannel channel = ManagedChannelBuilder.forTarget(ip + ":" + port.toString()).usePlaintext(true).build();
         NodeServiceGrpc.NodeServiceBlockingStub stub = NodeServiceGrpc.newBlockingStub(channel);
         p2p.nodes.Node.NodeMessage request = p2p.nodes.Node.NodeMessage.newBuilder().setPort(newPort).setId(newId).setIp(newIp).build();
@@ -182,7 +198,10 @@ public class NodeImpl extends NodeServiceGrpc.NodeServiceImplBase {
         this.nextNodePort = null;
         this.nextNodeIp = null;
         this.sensorSimulator = null;
-        sync = 0;
+        this.mustSendToken = false;
+        this.canReceiveToken = true;
+        syncNode = 0;
+        syncToken = 0;
     }
 
 }
